837e0d016ed7a66384b4e8260ed1e67e22c6f715
peraap <contact.peraapartments@gmail.com>
2026-04-06 19:58:48 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -15,9 +15,9 @@ declare global {
 const FB_PIXEL_ID = '1939919056723654';
 
 if (typeof window !== 'undefined') {
-  (function (f: any, b: any, e: any, v: any, n: any, t: any, s: any) {
+  (function(f: any, b: any, e: any, v: any, n: any, t: any, s: any) {
     if (f.fbq) return;
-    n = f.fbq = function () {
+    n = f.fbq = function() {
       n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
     };
     if (!f._fbq) f._fbq = n;
@@ -28,11 +28,11 @@ if (typeof window !== 'undefined') {
     t = b.createElement(e);
     t.async = !0;
     t.src = v;
-    s = b.getElementsByTagName(e)[0]; // SELECTEAZA PRIMUL SCRIPT
+    s = b.getElementsByTagName(e)[0];
     if (s && s.parentNode) {
       s.parentNode.insertBefore(t, s);
     }
-  })(window, document, 'script', 'https://facebook.net');
+  })(window, document, 'script', 'https://facebook.net'); // URL REPARAT AICI
 
   window.fbq('init', FB_PIXEL_ID);
   window.fbq('track', 'PageView');
