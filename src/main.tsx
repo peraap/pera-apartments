77d426e004fa49b4c02309265ebc898d8887a9ca
peraap <contact.peraapartments@gmail.com>
2026-04-06 19:29:40 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,41 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import ErrorBoundary from './components/ErrorBoundary';
 import './index.css';
+
+// --- CONFIGURARE META PIXEL ---
+declare global {
+  interface Window {
+    fbq: any;
+    _fbq: any;
+  }
+}
+
+const FB_PIXEL_ID = '1939919056723654';
+
+if (typeof window !== 'undefined') {
+  (function (f: any, b: any, e: any, v: any, n: any, t: any, s: any) {
+    if (f.fbq) return;
+    n = f.fbq = function () {
+      n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+    };
+    if (!f._fbq) f._fbq = n;
+    n.push = n;
+    n.loaded = !0;
+    n.version = '2.0';
+    n.queue = [];
+    t = b.createElement(e);
+    t.async = !0;
+    t.src = v;
+    s = b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t, s);
+  })(window, document, 'script', 'https://facebook.net');
+
+  window.fbq('init', FB_PIXEL_ID);
+  window.fbq('track', 'PageView');
+}
+// ------------------------------
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -11,3 +44,4 @@ createRoot(document.getElementById('root')!).render(
     </ErrorBoundary>
   </StrictMode>,
 );
+
