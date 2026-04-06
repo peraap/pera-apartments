c544282fc4f92712e390b8367c4943c7f4dbbb96
peraap <contact.peraapartments@gmail.com>
2026-04-06 20:13:29 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,34 +4,23 @@ import App from './App.tsx';
 import ErrorBoundary from './components/ErrorBoundary';
 import './index.css';
 
-declare global {
-  interface Window {
-    fbq: any;
-    _fbq: any;
-  }
-}
-
+// --- CONFIGURARE PIXEL ---
 const FB_PIXEL_ID = '1939919056723654';
 
 if (typeof window !== 'undefined') {
-  window.fbq = window.fbq || function() {
-    (window.fbq.q = window.fbq.q || []).push(arguments);
-  };
-  window._fbq = window._fbq || window.fbq;
-  window.fbq.push = window.fbq;
-  window.fbq.loaded = true;
-  window.fbq.version = '2.0';
-  window.fbq.queue = [];
+  // @ts-ignore
+  !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+  n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}
+  (window, document,'script','https://facebook.net');
 
-  const script = document.createElement('script');
-  script.async = true;
-  // URL-ul TREBUIE SA FIE EXACT ACESTA:
-  script.src = 'https://facebook.net';
-  document.head.appendChild(script);
-
+  // @ts-ignore
   window.fbq('init', FB_PIXEL_ID);
+  // @ts-ignore
   window.fbq('track', 'PageView');
 }
+// --------------------------
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
